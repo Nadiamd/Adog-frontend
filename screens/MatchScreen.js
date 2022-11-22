ab16b33dc44a89656f5b2cbe2af7b0dff191af0c
Hmidux <hameir.hmida@gmail.com>
2022-11-22 02:13:57 +0100

    Correction fichier MatchScreen pour le que le navigation.navigate cible la TabNavigator screen MailBox

--- screens/MatchScreen.js
+++ screens/MatchScreen.js
@@ -1,64 +1,72 @@
-import { Text,StyleSheet, Image, SafeAreaView , TouchableOpacity } from 'react-native'
-import React from 'react'
+import {
+  Text,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
 
-export default function MatchScreen({navigation}) {
-  
+export default function MatchScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-        <SafeAreaView style={styles.containerview}>
-        <Image source= {require("../images/sparkle-confetti.gif")} style={styles.confettigif}
-         />
+      <SafeAreaView style={styles.containerview}>
+        <Image
+          source={require("../images/sparkle-confetti.gif")}
+          style={styles.confettigif}
+        />
         <Text style={styles.match}> IT'S A MATCH ! </Text>
-        </SafeAreaView>
-    
-     
-      <Image source= {require("../images/husky-and-shiba-二哈萌柴2微信表情.gif")} 
-      style={styles.gif}
-     />
-     <TouchableOpacity onPress={() => navigation.navigate("Messagerie")}>
+      </SafeAreaView>
+
+      <Image
+        source={require("../images/husky-and-shiba-二哈萌柴2微信表情.gif")}
+        style={styles.gif}
+      />
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("TabNavigator", { screen: "MailBox" })
+        }
+      >
         <Image
           source={require("../images/patte-orange.png")}
           style={styles.imagePatte}
         />
       </TouchableOpacity>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "white",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
 
-    containerview:{
-
+  containerview: {
     marginBottom: "10%",
-    },
-    match : {
-        position: "absolute",
-        top:"35%",
-        color:"#F1890F",
-        fontSize:'50%',
-        marginLeft:'10%',
-       
-    },
-    gif:{
-        height:200,
-        width:250,
-        marginLeft:"20%",
-        marginBottom:"20%",
-    },
-    
-    confettigif:{
-        height:300,
-        width:400,
-    }, 
-    imagePatte:{
-        marginTop: "5%",
-        width: 60,
-        height: 60,
-        marginLeft:"40%",
-       
-    }
+  },
+  match: {
+    position: "absolute",
+    top: "35%",
+    color: "#F1890F",
+    fontSize: "50%",
+    marginLeft: "10%",
+  },
+  gif: {
+    height: 200,
+    width: 250,
+    marginLeft: "20%",
+    marginBottom: "20%",
+  },
+
+  confettigif: {
+    height: 300,
+    width: 400,
+  },
+  imagePatte: {
+    marginTop: "5%",
+    width: 60,
+    height: 60,
+    marginLeft: "40%",
+  },
 });
